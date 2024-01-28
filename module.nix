318c5a57a632fc3ec7fe528208a6e16dce2d1f3a
Justin Polis <j.polikevicius@gmail.com>
2024-01-28 16:19:40 +0200

    nix

--- module.nix
+++ module.nix
@@ -1,15 +1,36 @@
 { inputs, self, system }:
 { config, lib, pkgs, ... }:
-let cfg = config.programs.nordvpn;
+let
+  cfg = config.programs.nordvpn;
+  pkg = self.packages.${system}.default;
 in {
   options.programs.nordvpn = { enable = lib.mkEnableOption "nordvpn"; };
 
   config = lib.mkIf cfg.enable {
-    environment = { systemPackages = [ self.packages.${system}.default ]; };
+    environment = { systemPackages = [ pkg ]; };
     systemd.services.nordvpnd = {
+      description = "NordVPN daemon.";
+      serviceConfig = {
+        ExecStart = "${pkg}/bin/nordvpnd";
+        ExecStartPre = ''
+          ${pkgs.bash}/bin/bash -c '\
+            mkdir -m 700 -p /var/lib/nordvpn; \
+            if [ -z "$(ls -A /var/lib/nordvpn)" ]; then \
+              cp -r ${pkg}/var/lib/nordvpn/* /var/lib/nordvpn; \
+            fi'
+        '';
+        NonBlocking = true;
+        KillMode = "process";
+        Restart = "on-failure";
+        RestartSec = 5;
+        RuntimeDirectory = "nordvpn";
+        RuntimeDirectoryMode = "0750";
+        Group = "nordvpn";
+      };
       wantedBy = [ "multi-user.target" ];
-      serviceConfig.ExecStart =
-        "${self.packages.${system}.default}/bin/nordvpnd";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
     };
   };
 }
